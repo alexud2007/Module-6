a1f1ddd2f5a2f0cd312727ed6eb232e49c1106bb
Alexey Udivanov <alexud2007@gmail.com>
2023-02-15 21:47:00 +0300

    function Edit added & tested

--- index.php
+++ index.php
@@ -19,10 +19,23 @@ function remove(int $index, &$textStorage) {
     }
 }
 
+function edit(int $index, string $title, string $text, &$textStorage) {
+    if (isset($textStorage[$index])) {
+        $textStorage[$index]['title'] = $title;
+        $textStorage[$index]['text'] = $text;
+        return true;
+    } else {
+        return false;
+    }
+}
+
 addText('My first text', 'This is my first text', $textStorage);
 addText('My second text', 'This is my second text', $textStorage);
 
 remove(0, $textStorage); // true
 remove(5, $textStorage); // false
+
+edit(1, 'Updated second title', 'Updated second text', $textStorage); // true
+edit(3, 'Updated fourth title', 'Updated fourth text', $textStorage); // false
 
 print_r($textStorage);
