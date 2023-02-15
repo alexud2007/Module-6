59469302415ebbe074a72b0a550ea3f9ccd8ea0e
Alexey Udivanov <alexud2007@gmail.com>
2023-02-15 21:43:30 +0300

    function Remove added & tested

--- index.php
+++ index.php
@@ -10,7 +10,19 @@ function addText(string $title, string $text, &$textStorage)
     );
 }
 
+function remove(int $index, &$textStorage) {
+    if (isset($textStorage[$index])) {
+        unset($textStorage[$index]);
+        return true;
+    } else {
+        return false;
+    }
+}
+
 addText('My first text', 'This is my first text', $textStorage);
 addText('My second text', 'This is my second text', $textStorage);
+
+remove(0, $textStorage); // true
+remove(5, $textStorage); // false
 
 print_r($textStorage);
